c805c27995ba048ba13d413e6b1dd2bd65557db4
githmith <vuminhthanh2305@gmail.com>
2017-02-18 14:34:56 -0500

    Add reset method to Config

--- src/util/Config.java
+++ src/util/Config.java
@@ -8,8 +8,14 @@ import java.text.SimpleDateFormat;
  */
 public class Config {
 
-    public static int PROP_DELAY = 100;
-    public static int TRANS_DELAY_PER_BYTE = 1;
+    public static int PROP_DELAY = 1000;
+    public static int TRANS_DELAY_PER_BYTE = 10;
     public static double HTTP_VERSION = 1.1;
     public static final SimpleDateFormat HTTP_TIME_FORMAT = new SimpleDateFormat("dd.MM.yyyy hh:mm:ss");
+    
+    public static void reset(){
+        PROP_DELAY = 1000;
+        TRANS_DELAY_PER_BYTE = 10;
+        HTTP_VERSION = 1.1;
+    }
 }
