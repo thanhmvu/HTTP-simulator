ff800990ef290b3152e6bf4aeefa9aab8e3b5410
hongha912 <hongha912@gmail.com>
2017-02-18 10:08:36 -0500

    Modularize TransportLayer

--- src/lowerlayers/TransportLayer.java
+++ src/lowerlayers/TransportLayer.java
@@ -1,4 +1,5 @@
 package lowerlayers;
+
 import util.Config;
 
 public class TransportLayer {
@@ -9,6 +10,7 @@ public class TransportLayer {
 
     /**
      * Build transport layer
+     *
      * @param server server is true if the application is a server (should
      * listen) or false if it is a client (should try and connect)
      * @param propDelay Propagation delay (ms)
@@ -20,11 +22,78 @@ public class TransportLayer {
         connectionOpen = false;
     }
 
+    /**
+     * Send a payload
+     *
+     * @param payload The payload
+     * @throws InterruptedException
+     */
+    public void send(byte[] payload) throws InterruptedException {
+        handShakeIfNoConnection();
+        networkLayer.send(payload);
+
+        // if non-persistent, Close connection when client received the packet
+        if (Config.HTTP_VERSION == 1.0 && server) {
+            closeConnection();
+        }
+    }
+
+    public byte[] receive() throws InterruptedException {
+        handShakeIfNoConnection();
+        byte[] payload = networkLayer.receive();
+
+        // if non-persistent, Close connection when client received the packet
+        if (Config.HTTP_VERSION == 1.0 && !server) {
+            closeConnection();
+        }
+        return payload;
+    }
+
+    private void closeConnection() {
+        System.out.println(server ? "Server closes connection" : "Client closes connection");
+        connectionOpen = false;
+    }
+
+    private void handShakeIfNoConnection() throws InterruptedException {
+        if (!connectionOpen) {
+            if (!server) {
+                clientHandshakes();
+            } else {
+                serverHandshakes();
+            }
+        }
+
+    }
+
+    private void serverHandshakes() throws InterruptedException {
+        this.sendHandshakeProtocol("SYN");
+        this.listenForHandshake("ACK");
+        connectionOpen = true;
+    }
+
+    private void clientHandshakes() throws InterruptedException {
+        this.listenForHandshake("SYN");
+        connectionOpen = true;
+        this.sendHandshakeProtocol("ACK");
+    }
+
+    /**
+     * Send a handshake protocol to the other host
+     *
+     * @param protocol The phrasing of the protocol
+     * @throws InterruptedException
+     */
     private void sendHandshakeProtocol(String protocol) throws InterruptedException {
         networkLayer.send((protocol).getBytes());
         System.out.println(server ? "Server sends " + protocol : "Client sends " + protocol);
     }
 
+    /**
+     * Wait until receiving a handshake protocol from the other host
+     *
+     * @param protocol The phrasing of the protocol
+     * @throws InterruptedException
+     */
     private void listenForHandshake(String protocol) throws InterruptedException {
         while (true) {
             byte[] byteArray = networkLayer.receive();
@@ -34,51 +103,10 @@ public class TransportLayer {
             }
             String str = new String(byteArray);
             if (str.equals(protocol)) {
-                System.out.println(server ? "Server receives " + protocol: "Client receives " + protocol);
-
+                System.out.println(server ? "Server receives " + protocol : "Client receives " + protocol);
                 break;
             }
         }
 
-    }
-
-    private void handShake() throws InterruptedException {
-
-        if (!server) {
-            this.sendHandshakeProtocol("SYN");
-            this.listenForHandshake("ACK");
-            connectionOpen = true;
-        } else {
-            this.listenForHandshake("SYN");
-            connectionOpen = true;
-            this.sendHandshakeProtocol("ACK");
-        }
-    }
-    
-    public void send(byte[] payload) throws InterruptedException {
-        if (!connectionOpen) {
-            handShake();
-        }
-        networkLayer.send(payload);
-        
-        // if non-persistent, Close connection when client received the packet
-        if (Config.HTTP_VERSION == 1.0 && server){
-            System.out.println("Server closes connection");
-            connectionOpen = false;
-        }
-    }
-
-    public byte[] receive() throws InterruptedException {
-        if (!connectionOpen) {
-            handShake();
-        }
-        byte[] payload = networkLayer.receive();
-        
-        // if non-persistent, Close connection when client received the packet
-        if (Config.HTTP_VERSION == 1.0 && !server){
-            System.out.println("Client closes connection");
-            connectionOpen = false;
-        }
-        return payload;
     }
 }
