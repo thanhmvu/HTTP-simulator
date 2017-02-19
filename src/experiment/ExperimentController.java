d54228f89cf965e6b2a728d9e0c9118251cd8078
hongha912 <hongha912@gmail.com>
2017-02-19 17:29:28 -0500

    Uncomment ExpController

--- src/experiment/ExperimentController.java
+++ src/experiment/ExperimentController.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package experiment;
 
 import applayer.ClientApp;
@@ -20,7 +16,7 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
- *
+ * Control the experiment
  * @author thanhvu
  */
 public class ExperimentController {
